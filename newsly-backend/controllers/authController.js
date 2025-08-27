2ce445d910fde8d7ba1a5c725d17e65488a9b406
Srinivas Swaroop <cnsswaroop@gmail.com>
2025-08-28 00:48:03 +0530

    SignupAPI Using JWT

---
+++ newsly-backend/controllers/authController.js
@@ -0,0 +1,27 @@
+import User from "../models/User.js";
+import generateToken from "../utils/generateToken.js";
+
+// @desc    Signup user
+// @route   POST /api/auth/signup
+// @access  Public
+export const signup = async (req, res) => {
+  try {
+    const { name, email, password } = req.body;
+
+    const userExists = await User.findOne({ email });
+    if (userExists) {
+      return res.status(400).json({ message: "User already exists" });
+    }
+
+    const user = await User.create({ name, email, password });
+
+    res.status(201).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      token: generateToken(user._id),
+    });
+  } catch (err) {
+    res.status(500).json({ message: "Server error", error: err.message });
+  }
+};
